216f786efae29c7061f1e550e776b9f972c08214
Hao Quach <hgquach@uci.edu>
2017-01-21 14:54:04 -0800

    Setup blend tree and redid the background animation

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,28 +4,44 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
     public float playerSpeed;
-    public float moveX;
-    public float moveY;
+    private float moveX;
+    private float moveY;
+    private Vector2 movement;
 
-    private Rigidbody2D playerRigidbody; 
-
+    private Rigidbody2D playerRigidbody;
+    private Animator anim;
 	// Use this for initialization
 	void Start () {
         playerSpeed = 10f;
         playerRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("isMoving", false);
 
-        
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
 
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
+
         Vector2 movement = new Vector2(moveX,moveY);
 
         playerRigidbody.velocity = new Vector2(moveX * playerSpeed, moveY * playerSpeed);
-		
-	}
+		if(playerRigidbody.velocity == Vector2.zero)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            anim.SetBool("isMoving", true);
+            anim.SetFloat("moveX", movement.x);
+            anim.SetFloat("lastMoveX", movement.x);
+            anim.SetFloat("moveY", movement.y);
+            anim.SetFloat("lastMoveY", movement.y);
+
+        }
+    }
 }
