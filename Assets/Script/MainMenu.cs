4f7e39c51be6230ffc7cc13b6bf43b8737777945
Ian Tokuyama <igolftokuyama@gmail.com>
2017-01-20 23:54:21 -0800

    created main menu
    main menu scene

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public Button startText;
+    public Button exitText;
+
+    // Use this for initialization
+    void Start()
+    {
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        
+    }
+
+    public void ExitPress()
+    {
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
