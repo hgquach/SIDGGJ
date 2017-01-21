49c33a21f95a6946a164b7356b585d8b70a3922b
Steven Quinanola <ninjadude9190@sbcglobal.net>
2017-01-20 22:31:10 -0800

    Generators

---
+++ Assets/Script/Pulsation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulsation : MonoBehaviour {
+		int speed;
+		public Rigidbody2D rb2D; 
+	// Use this for initialization
+	void Start () {
+				rb2D = GetComponent<Rigidbody2D>();
+				speed = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				rb2d.MoveRotation(rb2D.rotation + speed * Time.fixedDeltaTime);
+	}
+}
