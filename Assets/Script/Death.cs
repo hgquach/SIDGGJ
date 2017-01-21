5cf65b898643c905aa0446e1735598f97fecbaa6
Steven Quinanola <ninjadude9190@sbcglobal.net>
2017-01-21 11:58:58 -0800

    Death script
    Player can die, needs animation? particles?

--- Assets/Script/Death.cs
+++ Assets/Script/Death.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Death : MonoBehaviour {
-
+		public bool isDying;
 	// Use this for initialization
 	void Start () {
-		
+				isDying = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+				if (isDying)
+				{
+						Destroy(gameObject);
+				}
 	}
+
+		void OnTriggerEnter2D(Collider2D other)
+		{
+				if (other.gameObject.CompareTag("Wave"))
+				{
+						
+						Destroy(this.gameObject);
+				}
+		}
 }
+
