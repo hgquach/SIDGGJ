9a171011d8145be497368fd949464a83448056c1
Steven Quinanola <ninjadude9190@sbcglobal.net>
2017-01-20 22:50:23 -0800

    Pulse Pounding Wave Wishes

--- Assets/Script/Pulsation.cs
+++ Assets/Script/Pulsation.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class Pulsation : MonoBehaviour {
 		int speed;
 		public Rigidbody2D rb2D; 
+		float percentScale;
+		bool increase = true;
 	// Use this for initialization
 	void Start () {
 				rb2D = GetComponent<Rigidbody2D>();
-				speed = 2;
+				speed = 20;
+				percentScale = 1.00f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-				rb2d.MoveRotation(rb2D.rotation + speed * Time.fixedDeltaTime);
+				rb2D.MoveRotation(rb2D.rotation + speed * Time.fixedDeltaTime);
+				rb2D.transform.localScale = new Vector3(percentScale, percentScale, 1);
+				if (increase)
+				{
+						percentScale += .05f;
+				}
+				else
+				{
+						percentScale -= .05f;
+				}
+				if (percentScale >= 1.5f || percentScale <= 0.5f)
+				{
+						increase = !increase;
+				}
 	}
 }
