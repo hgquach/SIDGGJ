aaba06b07e9633aa22c0768360e901b04536eb71
Hao Quach <hgquach@uci.edu>
2017-01-20 22:34:22 -0800

    Added simple gamestate object

---
+++ Assets/Script/GameState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+    public int numOfTotalCollect;
+    public int numOfCurrentCollect; 
+	// Use this for initialization
+	void Start () {
+        numOfTotalCollect = 3;
+        numOfCurrentCollect = 0; 
+		
+	}
+	
+	// Update is called once  per frame
+	void Update () {
+
+        if(numOfCurrentCollect == numOfTotalCollect)
+        {
+            print("Game Ended")
+        }
+		
+	}
+}
