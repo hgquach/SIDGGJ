889dab15a5cc7e719866ce0de13c8b7218c2defa
Hao Quach <hgquach@uci.edu>
2017-01-21 15:02:39 -0800

    hopefully fixed player controller

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,58 +4,39 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
     public float playerSpeed;
-<<<<<<< HEAD
     private float moveX;
     private float moveY;
     private Vector2 movement;
-=======
-    public float moveX;
-    public float moveY;
 	public float boostSpd;
-		public int boostTime;
-		public int boostDelay;
-		int boosting;
-		float boostMoveX;
-		float boostMoveY;
-
+	public int boostTime;
+	public int boostDelay;
+	int boosting;
+	float boostMoveX;
+	float boostMoveY;
 
     private Rigidbody2D playerRigidbody; 
->>>>>>> origin/master
 
-    private Rigidbody2D playerRigidbody;
     private Animator anim;
 	// Use this for initialization
 	void Start () {
         playerSpeed = 10f;
         playerRigidbody = GetComponent<Rigidbody2D>();
-<<<<<<< HEAD
         anim = GetComponent<Animator>();
         anim.SetBool("isMoving", false);
-
-
-
+        boostSpd = 3f;
+        boostTime = 7;
+        boostDelay = 5;
+        boosting = 0;
     }
 
-    // Update is called once per frame
-    void FixedUpdate () {
-=======
-		boostSpd = 3f;
-				boostTime = 7;
-				boostDelay = 5;
-				boosting = 0;
-		
-	}
-	
 	// Update is called once per frame
 	void FixedUpdate () {
->>>>>>> origin/master
 
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(moveX,moveY);
 
-<<<<<<< HEAD
         playerRigidbody.velocity = new Vector2(moveX * playerSpeed, moveY * playerSpeed);
 		if(playerRigidbody.velocity == Vector2.zero)
         {
@@ -70,31 +51,29 @@ public class PlayerControl : MonoBehaviour {
             anim.SetFloat("lastMoveY", movement.y);
 
         }
-    }
-=======
-				if (Input.GetKeyDown(KeyCode.Space) && boosting <= 0)
-				{
-						boosting = boostTime + boostDelay;
-						boostMoveX = moveX;
-						boostMoveY = moveY;
-				}
+
+		if (Input.GetKeyDown(KeyCode.Space) && boosting <= 0)
+		{
+				boosting = boostTime + boostDelay;
+				boostMoveX = moveX;
+				boostMoveY = moveY;
+		}
 		
-				if (boosting <= 0)
-				{
-						playerRigidbody.velocity = new Vector2(moveX * playerSpeed, moveY * playerSpeed);
-				}
-				else if (boosting > boostTime)
-				{
-						playerRigidbody.velocity = new Vector2(0, 0);
-						boosting--;
-				}
-				else
-				{
-						playerRigidbody.velocity = new Vector2(boostMoveX * playerSpeed * boostSpd, boostMoveY * playerSpeed * boostSpd);
-						boosting--;
-				}
+		if (boosting <= 0)
+		{
+				playerRigidbody.velocity = new Vector2(moveX * playerSpeed, moveY * playerSpeed);
+		}
+		else if (boosting > boostTime)
+		{
+				playerRigidbody.velocity = new Vector2(0, 0);
+				boosting--;
+		}
+		else
+		{
+				playerRigidbody.velocity = new Vector2(boostMoveX * playerSpeed * boostSpd, boostMoveY * playerSpeed * boostSpd);
+				boosting--;
+		}
         
 		
 	}
->>>>>>> origin/master
 }
