5ab35b14c443b7a6f0da1950fdc063804988740e
Ian Tokuyama <igolftokuyama@gmail.com>
2017-01-21 16:44:12 -0800

    Started Audio Manager GameObject

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Sound{
+
+    public string name;
+    public AudioClip clip;
+
+}
+
+public class AudioManager : MonoBehaviour {
+
+	
+}
