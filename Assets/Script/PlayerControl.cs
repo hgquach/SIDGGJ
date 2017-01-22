9acc4352e33b306648f41c820ad733f2a825fc3a
Hao Quach <hgquach@uci.edu>
2017-01-22 00:05:29 -0800

    Color trail adjusts to boost charge

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -3,30 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
-    public float playerSpeed;
     private float moveX;
     private float moveY;
     private Vector2 movement;
-	public float boostSpd;
+    private TrailRenderer tr;
+    private Rigidbody2D playerRigidbody;
+    private Animator anim;
+    private float boostMoveX;
+    private float boostMoveY;
+    private int charging;
+
+
+    public float boostSpd;
 	public int boostTime;
 	public int boostDelay;
-	int boosting;
-	float boostMoveX;
-	float boostMoveY;
-	int charges;
+    public float playerSpeed;
+    public int boosting;
+	public int charges;
 	public int maxCharges;
 	public int chargeRecharge;
-	int charging;
 
-    private Rigidbody2D playerRigidbody; 
-
-    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
         playerSpeed = 5f;
         playerRigidbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        tr = GetComponent<TrailRenderer>();
         anim.SetBool("isMoving", false);
         boostSpd = 6f;
         boostTime = 7;
@@ -36,71 +39,114 @@ public class PlayerControl : MonoBehaviour {
 		charges = maxCharges;
 		chargeRecharge = 100;
 		charging = 0;
+        
     }
-
-	// Update is called once per frame
-	void FixedUpdate () {
+    void Update()
+    {
 
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
 
-        Vector2 movement = new Vector2(moveX,moveY);
+        Vector2 movement = new Vector2(moveX, moveY);
 
-		if (Input.GetButtonDown("Jump") && boosting <= 0 && charges > 0)
-		{
-			boosting = boostTime + boostDelay;
-			boostMoveX = moveX;
-			boostMoveY = moveY;
-			charges--;
-		}
-		
-		if (boosting <= 0)
-		{
-			playerRigidbody.velocity = new Vector2(moveX * playerSpeed, moveY * playerSpeed);
-		}
-		else if (boosting > boostTime)
-		{
-				playerRigidbody.velocity = new Vector2(0, 0);
-				boosting--;
-		}
-		else
-		{
-				playerRigidbody.velocity = new Vector2(boostMoveX * playerSpeed * boostSpd, boostMoveY * playerSpeed * boostSpd);
-				boosting--;
-		}
-        
-
-		if(playerRigidbody.velocity == Vector2.zero)
-		{
-				anim.SetBool("isMoving", false);
-		}
-		else
-		{
-				anim.SetBool("isMoving", true);
-				anim.SetFloat("moveX", movement.x);
-				anim.SetFloat("lastMoveX", movement.x);
-				anim.SetFloat("moveY", movement.y);
-				anim.SetFloat("lastMoveY", movement.y);
-
-		}
-		
-		if(charging == chargeRecharge && charges < maxCharges)
-		{
-			charges++;
-			charging = 0;
-		}
-		if (charges < maxCharges)
-		{
-			charging++;
-		}
-
-		Vector2 moveDirection;  
-		Rigidbody2D rb2d = gameObject.GetComponent<Rigidbody2D>();
-		moveDirection = rb2d.velocity;
-		if (moveDirection != Vector2.zero)
+        if (Input.GetButtonDown("Jump") && boosting <= 0 && charges > 0)
         {
-			float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.AngleAxis(angle-90, Vector3.forward);
-		}
-	}
+            boosting = boostTime + boostDelay;
+            boostMoveX = moveX;
+            boostMoveY = moveY;
+            charges--;
+        }
+
+        if (boosting <= 0)
+        {
+            playerRigidbody.velocity = new Vector2(moveX * playerSpeed, moveY * playerSpeed);
+        }
+        else if (boosting > boostTime)
+        {
+            playerRigidbody.velocity = new Vector2(0, 0);
+            boosting--;
+        }
+        else
+        {
+            playerRigidbody.velocity = new Vector2(boostMoveX * playerSpeed * boostSpd, boostMoveY * playerSpeed * boostSpd);
+            boosting--;
+        }
+
+
+        if (playerRigidbody.velocity == Vector2.zero)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            anim.SetBool("isMoving", true);
+            anim.SetFloat("moveX", movement.x);
+            anim.SetFloat("lastMoveX", movement.x);
+            anim.SetFloat("moveY", movement.y);
+            anim.SetFloat("lastMoveY", movement.y);
+        }
+
+        if (charging == chargeRecharge && charges < maxCharges)
+        {
+            charges++;
+            charging = 0;
+        }
+        if (charges < maxCharges)
+        {
+            charging++;
+        }
+
+        // pasta code 
+        Vector2 moveDirection;
+        Rigidbody2D rb2d = gameObject.GetComponent<Rigidbody2D>();
+        moveDirection = rb2d.velocity;
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        }
+    
+    // code to change the trail color depedning on the charge left 
+    float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        switch (charges)
+        {
+            case 0:
+                gradient.SetKeys(
+                    new GradientColorKey[] {new GradientColorKey(Color.white, 0.0f),
+                    new GradientColorKey(Color.white, 39.4f),new GradientColorKey(Color.white, 95.6f)},
+                    new GradientAlphaKey[] {new GradientAlphaKey(alpha,0.0f), new GradientAlphaKey(alpha,48.5f),
+                    new GradientAlphaKey(169,98.5f) });
+                tr.colorGradient = gradient;
+                break;
+            case 1:
+                gradient.SetKeys(
+                    new GradientColorKey[] {new GradientColorKey(new Color(137,0,0), 0.0f),
+                    new GradientColorKey(new Color(240,56,56), 39.4f),new GradientColorKey(new Color(255,184,184), 95.6f)},
+                    new GradientAlphaKey[] {new GradientAlphaKey(alpha,0.0f), new GradientAlphaKey(alpha,48.5f),
+                    new GradientAlphaKey(169,98.5f) });
+                tr.colorGradient = gradient;
+                break;
+            case 2:
+                gradient.SetKeys(
+                    new GradientColorKey[] {new GradientColorKey(new Color(246,229,0), 0.0f),
+                    new GradientColorKey(new Color(248,243,102), 39.4f),new GradientColorKey(new Color(240,251,207), 95.6f)},
+                    new GradientAlphaKey[] {new GradientAlphaKey(alpha,0.0f), new GradientAlphaKey(alpha,48.5f),
+                    new GradientAlphaKey(169,98.5f) });
+                tr.colorGradient = gradient;
+                break;
+            case 3:
+                gradient.SetKeys(
+                    new GradientColorKey[] {new GradientColorKey(new Color(11,100,8), 0.0f),
+                    new GradientColorKey(new Color(24,182,0), 39.4f),new GradientColorKey(new Color(55,255,22), 95.6f)},
+                    new GradientAlphaKey[] {new GradientAlphaKey(alpha,0.0f), new GradientAlphaKey(alpha,48.5f),
+                    new GradientAlphaKey(169,98.5f) });
+                tr.colorGradient = gradient;
+                break;
+
+        }
+    }
+        // Update is called once per frame
+
+
 }
